af1180bf6fd416a144258e578f886692099fc8cc
jasonbanboa <jasonbanboa123@gmail.com>
2023-03-05 18:42:18 +0900

    details modal and rendering category

--- secondAttempt/js/common.js
+++ secondAttempt/js/common.js
@@ -1,17 +1,18 @@
 
 const $app = document.querySelector('#app');
- 
+const $detailDialog = document.querySelector('#post-detail-dialog');
 
 const fetchJSON = path => fetch(path).then(res => res.json());
-
 const POSTS = await fetchJSON('../../json/posts.json');
 const CATEGORIES = await fetchJSON('../../json/categorys.json');
 const MEMBERS = await fetchJSON('../../json/members.json');
-console.log({ POSTS, CATEGORIES, MEMBERS });
+
 
 // util  functions
-const getUser = memberIndex => MEMBERS.find(({ index }) => memberIndex === index);
-
+const getUser = (memberIndex) => MEMBERS.find(({ index }) => memberIndex === index);
+const getPost = (postId) => POSTS.find(({ index }) => index === postId);
+const getCategories = (categoryIndexArray) => CATEGORIES.filter(({ index }) => categoryIndexArray.includes(index));
+const makeCategoryInnerHTML = (categoriesArray) => categoriesArray.reduce((innerHTML, { name, color }) => innerHTML += `<span style="background-color: ${color}" class="category">${name}</span>`, '');
 // main
 
 const scrollStates = { 
@@ -40,7 +41,11 @@ window.onscroll = () => {
   }
 }
 
-function renderPosts({ title, contents, date, memberIndex }) {
+$detailDialog.addEventListener('click', (e) => {
+  if (e.target === $detailDialog) $detailDialog.close();
+})
+
+function renderPosts({ title, contents, date, memberIndex, categorys }) {
   const { name } = getUser(Number(memberIndex));
   const $post = document.createElement('article');
 
@@ -54,5 +59,29 @@ function renderPosts({ title, contents, date, memberIndex }) {
       <span class="timestamp">${date}</span>
     </section>
   `;    
+
+  $post.addEventListener('click', () => {
+    showPostDetailModal(title, contents, date, name, categorys);
+  });
   $app.appendChild($post);
 }
+
+function showPostDetailModal(title, contents, date, username, categorys) {
+  const categoriesArray = getCategories(categorys);
+  const categoryInnerHTML = makeCategoryInnerHTML(categoriesArray)
+  $detailDialog.showModal();  
+
+  $detailDialog.innerHTML = `
+    <div>
+      <div class="image"></div>
+      <h3 class="title">${title}</h3>
+      <p class="content">${contents}</p>
+      <section class="categories">${categoryInnerHTML}</section>
+      <section class="post-details">
+        <span class="created-by">${username}</span>
+        <span class="timestamp">${date}</span>
+      </section>
+    </div>
+  `;
+}
+
