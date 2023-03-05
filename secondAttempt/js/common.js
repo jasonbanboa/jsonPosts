4c239ae1cdcddcbae9d8d57a4597755289492343
jasonbanboa <jasonbanboa123@gmail.com>
2023-03-05 22:48:01 +0900

    filtering by category

--- secondAttempt/js/common.js
+++ secondAttempt/js/common.js
@@ -2,24 +2,17 @@
 const $app = document.querySelector('#app');
 const $detailDialog = document.querySelector('#post-detail-dialog');
 const $filterDialog = document.querySelector('#filter-dialog');
+const $filterByUsernameInput = $filterDialog.querySelector('#filter-by-username');
+const $filterByCategoriesInput = $filterDialog.querySelector('#filter-by-categories');
+const $cancleButton = document.querySelector('#cancel');
+const $filterButton = document.querySelector('#filter');
 
 const fetchJSON = path => fetch(path).then(res => res.json());
 const POSTS = await fetchJSON('../../json/posts.json');
 const CATEGORIES = await fetchJSON('../../json/categorys.json');
 const MEMBERS = await fetchJSON('../../json/members.json');
 
-
-// util  functions
-const getUser = (memberIndex) => MEMBERS.find(({ index }) => memberIndex === index);
-const getPost = (postId) => POSTS.find(({ index }) => index === postId);
-const getCategories = (categoryIndexArray) => CATEGORIES.filter(({ index }) => categoryIndexArray.includes(index));
-const makeCategoryInnerHTML = (categoriesArray) => categoriesArray.reduce((innerHTML, { name, color }) => innerHTML += `<span style="background-color: ${color}" class="category">${name}</span>`, '');
-
-function preventScroll(e) { e.preventDefault(); e.stopPropagation(); return false; }
-function disableScroll() { document.body.addEventListener('wheel', preventScroll, { passive: false }); }
-function enableScroll() { document.body.removeEventListener('wheel', preventScroll, { passive: false }); }
-
-// main
+// global states
 const modalStateObj = { 
   open: false,
 };
@@ -46,7 +39,30 @@ const scrollStates = {
   }
 };
 const keyPressStates = {};
+const filterStates = {
+  username: '',
+  categories: [],
+  reset: function() {
+    this.categories = [];
+    this.username = '';
+  }
+}
 
+// util  functions
+const getUser = (memberIndex) => MEMBERS.find(({ index }) => memberIndex === index);
+const getPost = (postId) => POSTS.find(({ index }) => index === postId);
+const getCategories = (categoryIndexArray) => CATEGORIES.filter(({ index }) => categoryIndexArray.includes(index));
+const makeCategoryInnerHTML = (categoriesArray) => categoriesArray.reduce((innerHTML, { name, color }) => innerHTML += `<span style="background-color: ${color}" class="category">${name}</span>`, '');
+const getUserByUsername = (username) => MEMBERS.find(({ name }) => name === username);
+const getCategoryByName = (categoryName) => CATEGORIES.find(({ name }) => name === categoryName); // currently not using
+const categoryNameTocategoryId = (categoryName) => CATEGORIES.find(({ name }) => name === categoryName).index;
+const isValidCategory = (category) => !filterStates.categories.includes(CATEGORIES.find(({ name }) => name === category).index); 
+
+function preventScroll(e) { e.preventDefault(); e.stopPropagation(); return false; }
+function disableScroll() { document.body.addEventListener('wheel', preventScroll, { passive: false }); }
+function enableScroll() { document.body.removeEventListener('wheel', preventScroll, { passive: false }); }
+
+// main
 
 POSTS.sort((a, b) => new Date(b.date) - new Date(a.date));
 POSTS.slice(0, 30).forEach(renderPosts);
@@ -80,12 +96,46 @@ window.addEventListener('keydown', (e) => {
 });
 
 $filterDialog.addEventListener('click', (e) => {
-  if (e.target === $filterDialog) {
+  if (e.target === $filterDialog || e.target === $cancleButton) {
     $filterDialog.close(); 
     modalState.open = false;
   }
 });
 
+// filter name only first
+$filterButton.addEventListener('click', () => {
+  const username = $filterByUsernameInput.value;
+  filterStates.username = username;
+
+  console.log(filterStates);
+  const filteredPosts = filterSearch(POSTS, filterStates);
+  console.log(filteredPosts);
+  
+  filterStates.reset();
+  modalState.open = false;
+  $filterDialog.close();
+});
+
+$filterByCategoriesInput.addEventListener('keydown', (e) => {
+  const { categories } = filterStates;
+  const { value } = $filterByCategoriesInput;
+  if (!value) return;
+  if (e.key === 'Enter' && isValidCategory(value.toLowerCase())) {
+    filterStates.categories = [...categories, categoryNameTocategoryId(value.toLowerCase())];
+    $filterByCategoriesInput.value = '';
+    console.log(filterStates);
+  }
+})
+
+// CURRENTLY ONLY FILTERS BY CATEGORIES
+function filterSearch(posts, { username, categories }) {
+
+  const data = posts.filter(({ categorys }) => categories.every(categoryID => categorys.includes(categoryID)));
+  return data;
+  // const { index: userID } = getUserByUsername(username);
+  // const data = posts.filter(({ memberIndex }) => memberIndex == userID);
+  // console.log(data);
+}
 
 function renderPosts({ title, contents, date, memberIndex, categorys }) {
   const { name } = getUser(Number(memberIndex));
@@ -127,7 +177,6 @@ function showPostDetailModal(title, contents, date, username, categorys) {
     </div>
   `;
 }
-
 
 
 
